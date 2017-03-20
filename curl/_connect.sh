bf879055bfcadc049ef14aa526c913cc3c27f328
uzquiano <mgu1@cornell.edu>
2017-03-20 13:46:19 -0400

    Update to use template variables

--- curl/_connect.sh
+++ curl/_connect.sh
@@ -3,16 +3,16 @@
 # the following keys were auto-populated for you by Cloud CMS when you
 # downloaded the developer's getting started kit
 #
-CLIENT_KEY={{clientKey}}
-CLIENT_SECRET={{clientSecret}}
-USERNAME={{username}}
-PASSWORD={{password}}
-BASE_URL={{baseURL}}
-REPOSITORY_ID={{repositoryId}}
-BRANCH_ID={{branchId}}
+CLIENT_KEY={{{clientKey}}}
+CLIENT_SECRET={{{clientSecret}}}
+USERNAME={{{username}}}
+PASSWORD={{{password}}}
+BASE_URL={{{baseURL}}}
+REPOSITORY_ID={{{repositoryId}}}
+BRANCH_ID={{{branchId}}}
 
 # tango mango node
-NODE_ID={{nodeId}}
+NODE_ID={{{nodeId}}}
 
 # connect via OAuth 2.0 and get an access token
 ACCESS_TOKEN_REQUEST_RESPONSE=$(curl -X POST -u "$CLIENT_KEY:$CLIENT_SECRET" --data-urlencode "grant_type=password" --data-urlencode "username=$USERNAME" --data-urlencode "password=$PASSWORD" "$BASE_URL/oauth/token")
